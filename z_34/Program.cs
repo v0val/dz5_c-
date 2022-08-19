e2c124153375bd7fee9452e768a131740e6b5568
valeryO <olennval@mail.ru>
2022-08-19 19:19:13 +0700

    number 34

---
+++ z_34/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите размер одномерного массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] mas = new int[n];
+Random rand = new Random();
+int kChet = 0;
+for (int i = 0; i < n; i++)
+{
+    mas[i] = rand.Next(100, 1000);
+    Console.Write($"{mas[i]} ");
+    if (mas[i] % 2 == 0) kChet += 1;
+}
+Console.WriteLine();
+Console.Write("Количество четных элементов массива: ");
+Console.WriteLine(kChet);
