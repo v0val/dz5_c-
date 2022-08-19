6e54888e82b9c90b0ff9c5be087987c66ef17c5d
valeryO <olennval@mail.ru>
2022-08-19 21:53:36 +0700

    number 36

---
+++ z_36/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите размер одномерного массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] mas = new int[n];
+Random rand = new Random();
+int SNechet = 0;
+for (int i = 0; i < n; i++)
+{
+    mas[i] = rand.Next(-10, 10);
+    Console.Write($"{mas[i]} ");
+    if (i % 2 != 0) SNechet += mas[i];
+}
+Console.WriteLine();
+Console.Write("Сумма  элементов массива стоящих на нечетных позициях: ");
+Console.WriteLine(SNechet);
