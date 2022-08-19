49ad24635d5296a63a6ffd29fb473861fcd9e59a
valeryO <olennval@mail.ru>
2022-08-19 22:00:24 +0700

    number 35

---
+++ z_35/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] mas = new int[12];
+Random rand = new Random();
+int countPosition = 0;
+for (int i = 0; i < 12; i++)
+{
+    mas[i] = rand.Next(-100, 100);
+    Console.Write($"{mas[i]} ");
+    if (mas[i] < 99 && mas[i] > 10) countPosition += 1;
+}
+Console.WriteLine();
+Console.Write("количество элементов массива, значения которых лежат в отрезке [10,99]: ");
+Console.WriteLine(countPosition);
